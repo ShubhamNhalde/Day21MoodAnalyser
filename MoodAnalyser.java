cffdb11a935ed50d3b8c1e90b0b021697af74766
Shubham <nhaldes@gmail.com>
2022-03-03 01:55:26 +0530

    Added: Mood Analyser Program

---
+++ MoodAnalyser.java
@@ -0,0 +1,13 @@
+package www.bridgelabz.moodanalyser;
+
+public class MoodAnalyser {
+	
+	public String analyseMood(String message) {
+		if (message.contains("sad")) {
+			return "SAD";
+
+		} else {
+			return "HAPPY";
+		}
+	}
+}
